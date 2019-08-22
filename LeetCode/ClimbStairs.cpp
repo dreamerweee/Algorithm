e291628708e8e151f82707f50703e6f2dbd94cc6
weee <wtdreamer@163.com>
2019-08-22 08:22:40 +0800

    ClimbStairs

--- LeetCode/ClimbStairs.cpp
+++ LeetCode/ClimbStairs.cpp
@@ -4,6 +4,29 @@
 * 注意：给定 n 是一个正整数。
 */
 
+// 递归，timeout
 int ClimbStairs(int n)
 {
+	if (n == 1)
+		return 1;
+	if (n == 2)
+		return 2;
+	return ClimbStairs(n - 1) + ClimbStairs(n - 2);
+}
+
+int ClimbStairs(int n)
+{
+	if (n == 1)
+		return 1;
+	if (n == 2)
+		return 2;
+	int ppre = 1;
+	int pre = 2;
+	int cur;
+	for (int i = 3; i <= n; ++i) {
+		cur = ppre + pre;
+		ppre = pre;
+		pre = cur;
+	}
+	return cur;
 }
