054a5e15edbfe4a75ed63f5777dbbb16e27a57b4
weee <wtdreamer@163.com>
2019-08-23 08:47:53 +0800

    EvalRPN

--- LeetCode/EvalRPN.cpp
+++ LeetCode/EvalRPN.cpp
@@ -6,6 +6,28 @@
 *	给定逆波兰表达式总是有效的。换句话说，表达式总会得出有效数值且不存在除数为 0 的情况。
 */
 
-int EvalRPN(vector<string>& tokens)
+int EvalRPN(vector<string> &tokens)
 {
+	stack<int> ret_stk;
+	for (auto s : tokens) {
+		if (s == "+" || s == "-" || s == "*" || s == "/") {
+			int calc1 = ret_stk.top();
+			ret_stk.pop();
+			int calc2 = ret_stk.top();
+			ret_stk.pop();
+			int res = 0;
+			if (s == "+")
+				res = calc1 + calc2;
+			else if (s == "-")
+				res = calc2 - calc1;
+			else if (s == "*")
+				res = calc1 * calc2;
+			else
+				res = calc2 / calc1;
+			ret_stk.push(res);
+		} else {
+			ret_stk.push(stoi(s));
+		}
+	}
+	return ret_stk.top();
 }
