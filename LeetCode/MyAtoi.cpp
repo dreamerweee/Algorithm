a145176affc634b8ef05443083016f006a0888f8
weee <wtdreamer@163.com>
2019-08-30 08:31:33 +0800

    MyAtoi

---
+++ LeetCode/MyAtoi.cpp
@@ -0,0 +1,46 @@
+/*
+* 请你来实现一个 atoi 函数，使其能将字符串转换成整数。
+* 首先，该函数会根据需要丢弃无用的开头空格字符，直到寻找到第一个非空格的字符为止。
+* 当我们寻找到的第一个非空字符为正或者负号时，则将该符号与之后面尽可能多的连续数字组合起来，作为该整数的正负号；
+* 假如第一个非空字符是数字，则直接将其与之后连续的数字字符组合起来，形成整数。
+* 该字符串除了有效的整数部分之后也可能会存在多余的字符，这些字符可以被忽略，它们对于函数不应该造成影响。
+* 注意：假如该字符串中的第一个非空格字符不是一个有效整数字符、字符串为空或字符串仅包含空白字符时，则你的函数不需要进行转换。
+* 在任何情况下，若函数不能进行有效的转换时，请返回 0。
+*/
+
+int MyAtoi(string str)
+{
+	size_t len = str.size();
+	long result = 0;
+	size_t idx = 0;
+	while (isspace(str[idx]))
+		++idx;
+
+	bool negative = false;
+	if (str[idx] == '-') {
+		negative = true;
+		++idx;
+	} else if (str[idx] == '+') {
+		++idx;
+	}
+
+	if (!isdigit(str[idx]))
+		return result;
+
+	for (; idx < len; ++idx) {
+		if (!isdigit(str[idx])) {
+			break;
+		} else {
+			result = result * 10 + str[idx] - '0';
+			if (negative) {
+				if (result > INT_MAX)
+					return INT_MIN;
+			} else {
+				if (result >= INT_MAX)
+					return INT_MAX;
+			}
+		}
+	}
+
+	return negative ? -result : result;
+}
