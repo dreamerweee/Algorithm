c919ae3c852c26b412954d8164b3a2e69e346d0c
weee <wtdreamer@163.com>
2019-09-06 08:00:42 +0800

    InvertTree

---
+++ LeetCode/InvertTree.cpp
@@ -0,0 +1,26 @@
+// 翻转一棵二叉树
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+TreeNode* InvertTree(TreeNode* root)
+{
+	if (root->left)
+		InvertTree(root->left);
+	if (root->right)
+		InvertTree(root->right);
+	if (!root->left && !root->right)
+		return root;
+	TreeNode *temp = root->left;
+	root->left = root->right;
+	root->right = temp;
+
+	return root;
+}
