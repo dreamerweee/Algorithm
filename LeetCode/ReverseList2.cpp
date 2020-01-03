efbd11825c25f7e666343678b8fd6ebd2c3c186e
weee <wtdreamer@163.com>
2020-01-03 18:14:03 +0800

    ReverseList2

--- LeetCode/ReverseList2.cpp
+++ LeetCode/ReverseList2.cpp
@@ -2,13 +2,14 @@
 * 反正单向链表和反正双向链表
 */
 
+// 单向链表
 Node* ReverseList(Node *head)
 {
   if (!head || !head->next) {
     return head;
   }
-  Node *prev = head;
-  Node *curr = head->next;
+  Node *prev = nullptr;
+  Node *curr = head;
   Node *next;
   while (curr) {
     next = curr->next;
@@ -17,5 +18,24 @@ Node* ReverseList(Node *head)
     curr = next;
   }
 
+  return prev;
+}
+
+// 双向链表
+DoubleNode* ReverseList(DoubleNode *head)
+{
+  if (!head || !head->next) {
+    return head;
+  }
+
+  DoubleNode *prev = nullptr;
+  DoubleNode *next = nullptr;
+  while (head) {
+    next = head->next;
+    head->next = prev;
+    head->prev = next;
+    prev = head;
+    head = next;
+  }
   return prev;
 }
